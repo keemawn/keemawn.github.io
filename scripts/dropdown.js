73d7b4a1e2b671662093c961f69856004b5d358d
keemawn <yipkaimen1304@gmail.com>
2024-06-30 20:44:53 +0800

    Cert drop down changed

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,8 +1,19 @@
-function toggleDropdown(id) {
+/*function toggleDropdown(id) {
     var element = document.getElementById(id);
     if (element.style.display === "none") {
         element.style.display = "block";
     } else {
         element.style.display = "none";
+    }
+}
+*/
+function toggleDropdown(id) {
+    const element = document.getElementById(id);
+    if (element.classList.contains('expanded')) {
+        element.classList.remove('expanded');
+        element.style.maxHeight = '0';
+    } else {
+        element.classList.add('expanded');
+        element.style.maxHeight = element.scrollHeight + 'px';
     }
 }
